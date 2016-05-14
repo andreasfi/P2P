887736df8185bab0c9ed0f52b03806949d0ae161
wbwriter <andreasfi@me.com>
2016-05-14 11:10:21 +0200

    Basic

---
+++ src/main/Client.java
@@ -0,0 +1,15 @@
+package main;
+/**
+ * Andreas Fischer
+ * P2P
+ * 14.05.2016
+ * Client.java
+ */
+
+/**
+ * @author Andreas
+ *
+ */
+public class Client {
+
+}
---
+++ src/main/Server.java
@@ -0,0 +1,15 @@
+/**
+ * Andreas Fischer
+ * P2P
+ * 14.05.2016
+ * Server.java
+ */
+package main;
+
+/**
+ * @author Andreas
+ *
+ */
+public class Server {
+
+}
