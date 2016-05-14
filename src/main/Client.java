84650ba2957ed4cae7ffb66f7552c12d0774e8f1
wbwriter <andreasfi@me.com>
2016-05-14 11:16:31 +0200

    Code

--- src/main/Client.java
+++ src/main/Client.java
@@ -6,10 +6,147 @@ package main;
  * Client.java
  */
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
 /**
  * @author Andreas
  *
  */
 public class Client {
+	private String serverIp;
+	private int serverPort;
+	List<SubClient> subClientList = new ArrayList<SubClient>();
+	
+	InetAddress myAdress;
+	private String myIp;
+	private String myName;
+	private List<File> myPath;
+	SubClient subclientMe;
+	
+	Scanner scanner = new Scanner(System.in);
+	Socket mySocket;
+	ObjectOutputStream objOutStr;
+	ObjectInputStream objInpStr;
+	OutputStream os ;
+	InetAddress LocalAddress;
+	public Client(){
+		myIp = "192.168.1.101";
+		myName = "Client name";
+	}
+	
+	public void connetToServer(String serverIp, int serverPort){
+		this.serverIp = serverIp;
+		this.serverPort = serverPort;
+		
+		try {
+			// Connect to server / Open socket
+			mySocket = new Socket(InetAddress.getByName(serverIp),45000);
+			try{
+				LocalAddress = InetAddress.getLocalHost();
+				System.out.println("The local address of the client is the following one : " +LocalAddress);
+				//System.out.println("My local address is the following one : " +LocalAddress);
+			}catch(UnknownHostException e){
+				e.printStackTrace();
+			}
+			System.out.println("The client is connected to " + serverIp);
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void sendSubClient(){
+
+		System.out.println("Path to folder you want to share :");
+		//String pathSharedFolder = scanner.nextLine();
+
+
+		String pathSharedFolder = "C:\\Users\\Andreas\\Dropbox\\HES-SO Valais\\2_Semester\\Semester 4\\Programmation Distribuée\\Projet\\Darlene\\ProgDistri_Darlene_CyrilS\\src\\client_server";
+
+		
+		//String pathSharedFolder = "C:\\temp";
+		File f = new File(pathSharedFolder);
+		myPath = new ArrayList<File>(Arrays.asList(f.listFiles()));
+		subclientMe = new SubClient(myIp, myName, myPath);
+		System.out.println("Subclient created: " + subclientMe.getIP());
+	}
+	public void sendObjectToServer(){
+		try {
+			os = mySocket.getOutputStream();
+			objOutStr = new ObjectOutputStream(os);
+			objOutStr.writeObject(subclientMe);
+			objOutStr.flush();
+			System.out.println("SubClient object sent.");			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public void getClientFileList(){
+		try {
+			
+			//objOutStr = new ObjectOutputStream(os);
+			//InputStream inputstream = mySocket.getInputStream();
+			objInpStr = new ObjectInputStream(mySocket.getInputStream());
+			// Read the object
+			subClientList = (List<SubClient>) objInpStr.readObject();
+			objOutStr.flush();
+			System.out.println("Object received.");
+			printSubclientList(subClientList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void closeConnection(){
+		try {
+			objOutStr.close();
+			objInpStr.close();
+			mySocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void printSubclientList(List<SubClient> subclientlist){
+		for (int i = 0; i < subclientlist.size(); i++) {
+			System.out.print("Name:" + subclientlist.get(i).getName());
+			System.out.print("IP:" + subclientlist.get(i).getIP());
+			for (int j = 0; j < subclientlist.get(i).getList().size(); j++) {
+				subclientlist.get(i).getList().get(j).toString();
+
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.connetToServer("192.168.108.10", 45000);
+		c.sendSubClient();
+		c.sendObjectToServer();
+		c.getClientFileList();
+	}
 
 }
--- src/main/Server.java
+++ src/main/Server.java
@@ -1,15 +1,180 @@
-/**
- * Andreas Fischer
- * P2P
- * 14.05.2016
- * Server.java
- */
 package main;
 
-/**
- * @author Andreas
- *
- */
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+
+
+public class Server implements Runnable {
+
+	Socket srvSocket = null ;
+	InetAddress localAddress = null;
+	ServerSocket mySkServer;
+	PrintWriter pout;
+	Scanner sc; 
+	int i =0;
+	String interfaceName = "eth1";
+	SubClient client_distant;
+	List<SubClient> SubClientList = new ArrayList<SubClient>();
+	String ipAddress;
+	ObjectInputStream inputStream;
+	ObjectOutputStream outputStream;
+	InputStreamReader isr;
+	String choice;
+
+	public void ServerClass()
+	{
+		SubClientList.add(new SubClient("129123", "asdfasfd", null));
+	}
+	
+	public void connect()
+	{
+		SubClient in = null;
+		
+		try {
+
+			NetworkInterface ni = NetworkInterface.getByName(interfaceName);
+			Enumeration<InetAddress> inetAddresses =  ni.getInetAddresses();
+			while(inetAddresses.hasMoreElements()) {
+				InetAddress ia = inetAddresses.nextElement();
+
+				if(!ia.isLinkLocalAddress()) {
+					if(!ia.isLoopbackAddress()) {
+						System.out.println(ni.getName() + "->IP: " + ia.getHostAddress());
+						localAddress = ia;
+					}
+				}   
+			}
+
+			//Warning : the backlog value (2nd parameter is handled by the implementation
+			mySkServer = new ServerSocket(45000,10,localAddress);
+
+			//set 3min timeout
+			mySkServer.setSoTimeout(180000);
+
+			System.out.println("Usedd IpAddress :" + mySkServer.getInetAddress());
+			System.out.println("Listening to Port :" + mySkServer.getLocalPort());
+
+			//wait for client connection		
+			srvSocket = mySkServer.accept(); 	
+			ipAddress = srvSocket.getRemoteSocketAddress().toString();
+			System.out.println(ipAddress + " is connected "+ i++);
+
+			//open the output data stream to write on the client
+			pout = new PrintWriter(srvSocket.getOutputStream()); 		  
+
+			inputStream = new ObjectInputStream(srvSocket.getInputStream());
+	        //outputStream = new ObjectOutputStream(srvSocket.getOutputStream());
+	        
+			in = (SubClient) inputStream.readObject();
+			SubClientList.add(new SubClient(in.getIP(), in.getName(), in.getList()));
+			
+			System.out.println(in.getIP());
+			
+			sendClientList();
+			//getClientChoice();
+			//giveClientIP();
+					
+			//Then die
+			System.out.println("Now dying");
+			srvSocket.close();
+			mySkServer.close();
+			pout.close();
+
+
+		}catch (SocketException e) {
+
+			System.out.println("Connection Timed out");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendClientList()
+	{
+		try {
+			outputStream = new ObjectOutputStream(srvSocket.getOutputStream());
+			System.out.println(SubClientList.get(0).getIP());
+			outputStream.writeObject(SubClientList);
+			outputStream.flush();
+			outputStream.close();
+			System.out.println("List send");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void giveClientIP()
+	{
+		SubClient clientChoosed = (SubClient) SubClientList.get(1);
+		clientChoosed.getIP();
+		
+		try {
+			
+			pout = new PrintWriter(srvSocket.getOutputStream());
+			pout.println(clientChoosed.getIP());
+			System.out.println("The ip send to connect to the client is " + clientChoosed.getIP());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void getClientChoice()
+	{
+		
+		try {
+			BufferedReader buffin = new BufferedReader (new InputStreamReader (srvSocket.getInputStream()));
+			choice = buffin.readLine();
+			System.out.println("Your choice is" + choice);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	 
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Server server = new Server();
+		
+		server.connect();
+		
+	}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Runnable#run()
+		 */
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
 
 }
